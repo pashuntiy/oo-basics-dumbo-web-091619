645a6666c35202e77c035443bbc4723178b6846f
Pavel Ilin <pavel_ilin@yahoo.com>
2019-09-18 17:48:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,14 @@
 
 class Shoe
   attr_accessor :brand, :color, :size, :size, :material, :condition
+  attr_writer
   
   def initialize(brand)
     @brand = brand
   end
   
   def cobble
-    
+    self.
   end
   
 end
